ec307536afd0bdf257b83aafbe8db55925cad733
icrucialx <avion.blueberry@gmail.com>
2025-01-23 14:46:58 -0600

    Update server.py

--- server.py
+++ server.py
@@ -9,25 +9,22 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-@app.route('/cards', methods=['GET'])
-def get_cards():
-    conn = get_db_connection()
-    cards = conn.execute('SELECT * FROM cards').fetchall()
-    conn.close()
-    return jsonify([dict(card) for card in cards])
-
+# API endpoint to log a card pull
 @app.route('/pulls', methods=['POST'])
-def add_pull():
-    new_pull = request.get_json()
-    card_name = new_pull['card_name']
-    rarity = new_pull['rarity']
+def log_pull():
+    data = request.get_json()
+    card_name = data['card_name']
+    rarity = data['rarity']
     date_of_pull = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     conn = get_db_connection()
-    conn.execute('INSERT INTO pulls (card_name, rarity, date_of_pull) VALUES (?, ?, ?)',
-                 (card_name, rarity, date_of_pull))
+    conn.execute(
+        'INSERT INTO pulls (card_name, rarity, date_of_pull) VALUES (?, ?, ?)',
+        (card_name, rarity, date_of_pull)
+    )
     conn.commit()
     conn.close()
+
     return jsonify({'status': 'success'}), 201
 
 if __name__ == '__main__':
